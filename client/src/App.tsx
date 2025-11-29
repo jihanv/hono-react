690d727d9b897eb323f9340ef0a3da4cd8fb02d4
jihanv <hedjmny@gmail.com>
2025-11-29 20:24:13 +0900

    Have client make a fetch call to the server.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
-
+import { useEffect, useState } from "react";
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+};
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch("api/books");
+      const data = await response.json();
+      setBooks(data);
+    };
+    fetchBooks();
+  });
 
   return (
     <>
-      <div>Hello</div>
+      <div>
+        Book List
+        <ul>
+          {books.map((book: Book) => (
+            <li key={book.id}>{book.title} by [book.author]</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
