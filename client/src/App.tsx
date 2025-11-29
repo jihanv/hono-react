8041e0ff9e56cf3fb70b957f3af247f45198c257
jihanv <hedjmny@gmail.com>
2025-11-29 21:00:55 +0900

    Fix typo in index.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ const client = hc<AppType>("/")
 
 function App() {
   const [books, setBooks] = useState<InferResponseType<typeof client.api.books.$get>>([]);
+  const [count, setCount] = useState(0)
   useEffect(() => {
     const fetchBooks = async () => {
       // const response = await fetch("api/books");
@@ -19,13 +20,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Book List</h1>
-        <ul>
+      <div className="p-20">
+        <h1 className="flex justify-center text-4xl m-4">Book List</h1>
+        <ul className="list bg-base-200 rounded-box shadow-md">
           {books.map((book) => (
-            <li key={book.id}>{book.title} by {book.author}</li>
+            <li key={book.id} className="list-row">{book.title} by {book.author}</li>
           ))}
         </ul>
+        <div className="flex justify-end">
+          <button
+            className="btn btn-primary mt-4"
+            onClick={() => {
+              setCount((prev) => prev + 1)
+            }}>
+            Click me! {count}
+          </button>
+        </div>
       </div>
     </>
   );
